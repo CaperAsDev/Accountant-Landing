0269aeb131939dc998ee4d9c3822092a53887204
Caperactus <102335770+CaperAsDev@users.noreply.github.com>
2024-05-04 18:13:35 -0500

    modify vite config again

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,24 @@ import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    TanStackRouterVite(),
-  ],
+export default defineConfig(({
+  command,
+}) => {
+  if (command === 'serve') {
+    return {
+      plugins: [
+        react(),
+        TanStackRouterVite(),
+      ],
+      // dev specific config
+    };
+  }
+  // command === 'build'
+  return {
+    plugins: [
+      react(),
+      TanStackRouterVite(),
+    ],
+    base: '/Accountant-Landing/',
+  };
 });
